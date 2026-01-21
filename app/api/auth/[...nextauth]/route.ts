56faf775bf47d1f5f1d4b589a1cfc5584240d29e
James <jameson.olitoquit@gmail.com>
2026-01-21 21:11:36 +0800

    Fix PrismaClient import for Railway deployment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import PrismaClient from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
